f6472c4b314ad321fa79c2b44b1b7cb610737724
Yeab-tilahun <yeabsiratilahun78@gmail.com>
2023-10-24 00:12:05 +0300

    C-20: Done with custome hooks

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import { Route, Routes, useNavigate } from 'react-router-dom'
 import { format } from 'date-fns';
 import api from './api/posts'
 import EditPost from './EditPost'
+import useWindowSize from './hooks/useWindowSize'
+import useAxiosFetch from './hooks/useAxiosFetch';
 function App() {
   const [posts, setPosts] = useState([])
   const [search, setSearch] = useState('')
@@ -21,26 +23,15 @@ function App() {
 
   const [editTitle, setEditTitle] = useState('')
   const [editBody, setEditBody] = useState('')
+  const { width } = useWindowSize()
 
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get('/posts');
-        setPosts(response.data);
-      } catch (err) {
-        if (err.response) {
-          // Not in the 200 response range 
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    }
+    setPosts(data);
+  }, [data])
 
-    fetchPosts();
-  }, [])
+
+
   useEffect(() => {
     const filteredResult = posts.filter((post) => (
       (post.body).toLowerCase()).includes(search.toLowerCase())
@@ -92,11 +83,17 @@ function App() {
 
   return (
     <div className='App'>
-      <Header title='Yeabs Blog' />
+      <Header title='Yeabs Blog' width={width} />
       <Nav search={search} setSearch={setSearch} />
 
       <Routes>
-        <Route path='/' element={<Home posts={searchResult} />} />
+        <Route path='/' element={
+          <Home
+            posts={searchResult}
+            fetchError={fetchError}
+            isLoading={isLoading}
+          />}
+        />
         <Route exact path='/post' element={
           <NewPost
             postTitle={postTitle}
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from 'react-icons/fa'
 
-function Header({ title }) {
+function Header({ title, width }) {
     return (
-        <header className='Header'><h1>{title}</h1></header>
+        <header className="Header">
+            <h1>{title}</h1>
+            {width < 768 ? <FaMobileAlt />
+                : width < 992 ? <FaTabletAlt />
+                    : <FaLaptop />}
+        </header>
     )
 }
 
